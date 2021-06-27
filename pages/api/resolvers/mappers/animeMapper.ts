d4c752741104878d7dae4f090fe2fb4f7b357bfe
Michael Lyon <mlyon360@gmail.com>
2021-06-27 13:10:13 -0600

    fix lint errors and handle more empty data cases

--- pages/api/resolvers/mappers/animeMapper.ts
+++ pages/api/resolvers/mappers/animeMapper.ts
@@ -1,9 +1,11 @@
 import { AnimeEntity } from 'pages/api/datasources/dynamodb'
 
-export default (animeEntity: AnimeEntity): Anime => {
+const animeMapper = (animeEntity: AnimeEntity): Anime => {
   const { PK, SK, GSI1PK, GSI1SK, GSI2PK, GSI2SK, ...rest } = animeEntity
 
   const anime = { ...animeEntity }
 
   return anime
 }
+
+export default animeMapper
