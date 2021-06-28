56332884fa4ffa6584318cd135c8efcd7df627a2
Michael Lyon <mlyon360@gmail.com>
2021-06-28 14:02:25 -0600

    seperate out anime components from page

---
+++ components/anime-page/index.ts
@@ -0,0 +1,15 @@
+import StatusBadge from './StatusBadge'
+import GenreTag from './GenreTag'
+import SeasonTag from './SeasonTag'
+import TypeBadge from './TypeBadge'
+import CoverImage from './CoverImage'
+import MoreInformationColumn from './MoreInformationColumn'
+
+export {
+  StatusBadge,
+  GenreTag,
+  SeasonTag,
+  TypeBadge,
+  CoverImage,
+  MoreInformationColumn,
+}
